6c3b2bd9dfe09dbd83bd67c3af0098af8b5f5ece
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-12 12:29:09 +0200

    finito

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,3 +1,16 @@
 /* Snack 3
 Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti. */
+
+const number1 = prompt("Dammi un numero");
+const number2 = prompt("Dammi un numero");
+const number3 = prompt("Dammi un numero");
+const number4 = prompt("Dammi un numero");
+const number5 = prompt("Dammi un numero");
+const number6 = prompt("Dammi un numero");
+const number7 = prompt("Dammi un numero");
+const number8 = prompt("Dammi un numero");
+const number9 = prompt("Dammi un numero");
+const number10 = prompt("Dammi un numero");
+
+console.log(Number(number1) + Number(number2) + Number(number3) + Number(number4) + Number(number5) + Number(number6) + Number(number7) + Number(number8) + Number(number9) + Number(number10));
