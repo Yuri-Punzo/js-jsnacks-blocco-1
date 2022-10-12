6cb1c1cf29c5d19c5da01d41945bd596a33325ce
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-12 12:42:52 +0200

    prova con ciclo incompleta

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -2,7 +2,7 @@
 Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti. */
 
-const number1 = prompt("Dammi un numero");
+/* const number1 = prompt("Dammi un numero");
 const number2 = prompt("Dammi un numero");
 const number3 = prompt("Dammi un numero");
 const number4 = prompt("Dammi un numero");
@@ -13,4 +13,12 @@ const number8 = prompt("Dammi un numero");
 const number9 = prompt("Dammi un numero");
 const number10 = prompt("Dammi un numero");
 
-console.log(Number(number1) + Number(number2) + Number(number3) + Number(number4) + Number(number5) + Number(number6) + Number(number7) + Number(number8) + Number(number9) + Number(number10));
+console.log(Number(number1) + Number(number2) + Number(number3) + Number(number4) + Number(number5) + Number(number6) + Number(number7) + Number(number8) + Number(number9) + Number(number10)); */
+
+
+/* seconda versione con ciclo for */
+
+for (let i = 1; i <= 10; i++){
+    let askNumber = prompt("Dammi un numero");
+    console.log(askNumber);
+}
