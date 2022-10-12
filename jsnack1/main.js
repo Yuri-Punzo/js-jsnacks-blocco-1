4f1c0ed15e526abe8dc56e69635e7dc19613564f
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-12 11:44:43 +0200

    added some ; and indent

--- jsnack1/main.js
+++ jsnack1/main.js
@@ -3,16 +3,15 @@ L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore. */
 
 const firstNumber = prompt("Dammi un numero");
-
 const secondNumber = prompt("Dammi un altro numero");
+let higherNumber;
 
-let higherNumber
 if (firstNumber > secondNumber) {
     console.log(firstNumber);
-    higherNumber = firstNumber
+    higherNumber = firstNumber;
 } else if (firstNumber < secondNumber) {
     console.log(secondNumber);
-    higherNumber = secondNumber
+    higherNumber = secondNumber;
 }
 
-document.getElementById("higher_number").innerHTML = higherNumber
+document.getElementById("higher_number").innerHTML = higherNumber;
